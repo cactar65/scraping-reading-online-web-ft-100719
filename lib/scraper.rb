54a6c68322588fc1f074c3a6814d6d0cf1d2b628
Nicholas Walton <cactar65@gmail.com>
2019-11-01 04:42:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,8 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-260IBN").text
 
-courses = doc.css
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |courses|
+  puts courses.text.strip
+end
